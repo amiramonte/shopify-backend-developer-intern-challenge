4fb0434a659f21c1c2cb39e2b43ae15ae56e0dda
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-05-13 16:26:11 -0700

    added pseudocode.js file and updated it with preliminary breakdown of my next steps for building the backend code

--- z-pseudocode.js
+++ z-pseudocode.js
@@ -1,13 +1,35 @@
+// Build Models
+    // build surfboard model with necessary information for each surfboard
+        // length, volume, manufacturer, location tied to
+        // 
+    // build warehouse/location model 
+        // location name and inventory for each one
+        // main warehouse and smaller surf shops 
 // 
+    // index.js file
+    // import models
+    // set up table associations for surfboards and warehouses/locations
+    // export models
+    // 
 // 
+// Build Seed data
+    // seed.js file
+        // import models
+        //  import sequelize connection
+        //  create surfboard seed data
+        //  create warehouse seed data
+        //  async await function to seed data
+        //  
+        //  
+        //  
+        //  
 // 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+// Controllers folder
+    // index.js file
+        // set up express router
+        // 
+    // api folder
+        // set up express router
+        // define api route variables for use - surfboards and warehouses/locations
+        // 
 // 
