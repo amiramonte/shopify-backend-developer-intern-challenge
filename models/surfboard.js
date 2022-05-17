bd37fcd8bf4a94223b5c99c8ce21ae34bae2f42a
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-05-17 15:30:29 -0700

    added location-routes.js and surfboard-routes.js files to api folder nested under controllers folder

--- models/surfboard.js
+++ models/surfboard.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes} = require("sequelize");
+const {Model, DataTypes} = require("sequelize");
 const sequelize = require("../config/configuration");
 
 class Surfboard extends Model {}
