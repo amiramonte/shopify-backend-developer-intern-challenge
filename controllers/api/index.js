5242fc6dc14909b61673c6f17832c00fbad8cbd0
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-05-17 15:35:07 -0700

    defined and set up routes for locations and surfboards in index.js file of api folder, nested in controllers folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,12 @@
+// Importing Router set up
 const router = require("express").Router();
+
+// Defining Variables for Routes
+const locationRoutes = require("./location-routes");
+const surfboardRoutes = require("./surfboard-routes");
+
+// Routes
+router.use("/location", locationRoutes);
+router.use("/surfboard", surfboardRoutes);
 
 module.exports = router;
