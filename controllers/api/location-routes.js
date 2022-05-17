3113994a129e87688d9823225cd0e65928c4f11c
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-05-17 15:52:27 -0700

    added get all locations route in location-routes.js file

--- controllers/api/location-routes.js
+++ controllers/api/location-routes.js
@@ -5,5 +5,18 @@ const {Surfboard, Location} = require("../../models");
 // The `/api/location` endpoint
 
 // GET route for all locations and associated surfboards
+router.get('/getlocations', async(req, res) => {
+    try {
+        const getLocations = await Location.findAll({
+            include: [Surfboard]
+        })
+        
+        res.status(200).json(getLocations);
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json(error);
+    }
+})
 
 module.exports = router;
