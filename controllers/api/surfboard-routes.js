16ac00f3645b46827deb21bd395bc83deda61f1f
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-05-17 15:54:51 -0700

    added get all surfboards route to surboard-routes.js file

---
+++ controllers/api/surfboard-routes.js
@@ -0,0 +1,22 @@
+// Imports 
+const router = require('express').Router();
+const {Surfboard, Location} = require("../../models");
+
+// The `/api/surfboard` endpoint
+
+// GET route for all surfboards and associated locations
+router.get('/getallsurfboards', async(req, res) => {
+    try {
+        const allSurfboards = await Surfboard.findAll({
+            include: [Location]
+        })
+        
+        res.status(200).json(allSurfboards);
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json(error);
+    }
+})
+
+module.exports = router;
